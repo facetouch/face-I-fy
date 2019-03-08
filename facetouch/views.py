37549ffa9f5ad09696fc5a4d16bca395813cfab1
anipmehta <anipmehta13@gmail.com>
2019-03-08 11:33:12 -0500

    fixing continous scrolling issue

--- facetouch/views.py
+++ facetouch/views.py
@@ -10,9 +10,15 @@ def create_event(request):
     if request.method == 'POST':
         # import pdb;pdb.set_trace()
         data = json.loads(request.body)
+        event_name = data['event_name']
+        if "move_left" or "move_right" or "move_up" or "move_down" in event_name:
+            events = Event.objects.filter(event_name=data['event_name'], is_consumed=False)
+            if len(events) > 0:
+                return JsonResponse({'status': 200})
         event = Event(event_name=data['event_name'], time_stamp=datetime.utcnow())
         event.save()
     return JsonResponse({'status': 200})
+
 
 @csrf_exempt
 def get_event(request):
