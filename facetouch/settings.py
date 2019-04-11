45f9b80d8f3f88c81bddaf4600d3b335febfd883
abhishekparekh1 <abhishekparekh1@gmail.com>
2019-04-11 18:46:01 -0400

    Front End Styling

--- facetouch/settings.py
+++ facetouch/settings.py
@@ -126,7 +126,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
